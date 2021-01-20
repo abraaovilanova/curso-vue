8bff1b282148e0dbf302108d618e174276753d4d
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-01-20 16:47:40 -0300

    Usando condicionais e Listas

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        livro: {
+            titulo: 'O Senhor dos Anéis',
+            autor: 'J. R. R. Tolkien',
+            volume: '3'
+        },
+        aluno: {
+            id: 10,
+            nome: 'Maria',
+            notas: [7.67, 8.33, 6.98, 9.21]
+        },
+        despesas: {
+            id: 1,
+            tipo: 'Fixa',
+            total: 3000,
+
+        },
+        alterar: true,
+        minhaLista: ['Abraão', 'Ana Paula', 'Helena'],
+    }
+
+})
