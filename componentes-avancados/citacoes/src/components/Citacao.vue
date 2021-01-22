dad005c40d314c4a9684229e76b78df3ab552af1
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-01-22 15:35:58 -0300

    Componentes avançados

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="citacao">
+        <div class="autor">
+            <slot name="autor"></slot>
+        </div>
+        <slot></slot>
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log('created')
+    },
+    destroyed(){
+        console.log('destroyed')
+    },
+    activated(){
+        console.log('activated')
+    },
+    deactivated(){
+        console.log('deactivated')
+    },
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    .autor {
+        float:right;
+    }
+
+</style>
