604817cac89ec6607e0eb0372cc5adced8735c02
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-01-27 18:17:08 -0300

    fim do modulo 11

--- formularios/formulario-desafio/src/App.vue
+++ formularios/formulario-desafio/src/App.vue
@@ -2,26 +2,37 @@
 	<div id="app">
 		<h1>Formulário Desafio</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
-				<!-- Exercicio 01 -->
-				<!-- Criar uma formulário de registro -->
-				<!-- Nome completo (Nome e Sobrenome) -->
-				<!-- Email -->
-				<!-- Senha -->
-				<!-- Armazenar Dados? (Sim/Não) -->
-
-				<!-- Exercicio 02 -->
-				<!-- Só mostrar o fomulário de não tiver sido submetido -->
-				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
-				
-				<!-- Exercicio 03 -->
-				<!-- Crie um componente personalizado NomeCompleto -->
-				<!-- Esse componente deve receber Nome e Sobrenome -->
+				<NomeCompleto v-model="nomeCompleto" />
+				<Rotulo nome="Email">
+					<input type="text===" v-model="email" />
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="senha" />
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados">
+					<input type="checkbox" v-model="armazenarDados"/>
+				</Rotulo>
+				<button @click.prevent="enviar">Enviar</button>
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
-
+				<Rotulo nome="Nome">
+					<span> {{ nomeCompleto.nome }} </span>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<span> {{ nomeCompleto.sobrenome }} </span>
+				</Rotulo>
+				<Rotulo nome="Email">
+					<span> {{ email }} </span>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<span> {{ senha }} </span>
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados">
+					<span> {{ armazenarDados }} </span>
+				</Rotulo>
 			</div>
 		</div>
 	</div>
@@ -29,10 +40,29 @@
 
 <script>
 import Rotulo from './components/Rotulo.vue'
+import NomeCompleto from './components/NomeCompleto.vue'
 
 export default {
 	name: 'app',
-	components: { Rotulo }
+	components: { Rotulo, NomeCompleto },
+	data(){
+		return {
+			enviado: false,
+			nomeCompleto: {
+				nome: '',
+				sobrenome: '',
+			},
+			email:'',
+			senha:'',
+			armazenarDados: true
+		}
+	},
+	methods: {
+		enviar() {
+			this.enviado = true
+		}
+	}
+
 }
 </script>
 
